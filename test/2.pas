5ef140e1fa7f62597000272a51dd40a40008fc0d
weihao <weihaowork@outlook.com>
2020-07-19 09:51:23 +0800

    test

--- test/2.pas
+++ test/2.pas
@@ -1,7 +1,33 @@
-program test(input,output);
-const cn = 2; 
-    dn = 123.23;
-var x:integer;
+program sort(input,output);
+
+var i,x:integer;
+    a:array[0..100] of integer;
+
+procedure quiksort(low,high:integer);
+    var i,j,mid,temp:integer;
+    begin
+        i:=low;
+        j:=high;
+        mid:=a[(i+j)div 2];
+        repeat
+            while a[i]<mid do i:=i+1;
+            while mid<a[j] do j:=j-1;
+            if i<=j then
+            begin
+                temp:=a[i];
+                a[i]:=a[j];
+                a[j]:=temp;
+            end;
+        until (i>j);
+        if i<high then quiksort(i,high);
+        if j>low then quiksort(low,j);
+    end;
+
 begin
-    x:=cn;
+    read(n);
+    for i:=1 to n do
+        read(a[i]);
+    quiksort(i,n);
+    for i:=1 to n do
+        write(a[i], ' ');
 end.
